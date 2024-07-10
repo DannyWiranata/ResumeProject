246b5659e0afbfcdef83367e456c81517a6fb475
DannyWiranata <dannywiranata24@gmail.com>
2024-07-10 11:34:57 +0800

    reverse cypher.cpp

--- reverse cypher.cpp
+++ reverse cypher.cpp
@@ -1,1 +1,23 @@
+#include <iostream>
+#include <string>
 
+using namespace std;
+
+int main() {
+    string pesan;
+    getline(cin, pesan);
+    cout << "Masukkan pesan: " << pesan << endl;
+    cout << "" << endl;
+
+    int ckey;
+    cin >> ckey;
+    cout << "Masukkan Key Caesar Cipher: " << ckey << endl;
+    cout << "" << endl;
+
+    cout << "Hasil Enkripsi dengan Reverse Cipher: ";
+    int indeks = pesan.length() - 1;
+    while (indeks >= 0) {
+        cout << pesan[indeks];
+        indeks-=1;
+    }
+}
